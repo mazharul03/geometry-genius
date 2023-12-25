922d682a8588bd4b73765974e62e3e29f436aca9
Mazharul Islam <mazharul3islam0@gmail.com>
2023-12-25 12:16:36 +0600

    push

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -33,6 +33,12 @@ function calculateRectangleArea() {
     const length = parseFloat(lengthValueText);
     console.log(length);
 
+    // validate input: width and length
+    if (isNaN(width) || isNaN(length)) {
+        alert('please insert a number');
+        return;
+    }
+
     // calculate area
     const area = width * length;
 
@@ -41,3 +47,63 @@ function calculateRectangleArea() {
     rectangleAreaSpan.innerText = area;
 
 }
+
+
+// reusable function --> reduce duplicate code
+
+function calculateParallelogramArea() {
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('parallelogram-height');
+
+    // validate
+
+    if (isNaN(base) || isNaN(height)) {
+        alert('please insert a number');
+        return;
+    }
+
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+}
+
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+    const area = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', area);
+
+}
+
+// reusable get input value field in number
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+
+}
+
+// reusable set span, p, div etc text
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+
+
+}
+
+// add to calculation entry
+
+function addToCalculationEntry(areaType, area) {
+    console.log('adding soon')
+}
+
+
+// Data validation
+
+/* 
+1. set the proper type of the input field. (number, date, email)
+2. check type using typeof
+3. NaN means : Not a number. isNaN is checking whether the input is not a number or not.
+
+
+*/
